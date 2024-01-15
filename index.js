4ab0b00a1fe983f7a432133bd3951a4f75a94fcf
Ratan Hussain <mdratanmia7395@gmail.com>
2024-01-15 16:01:05 +0300

    added some code

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const participentName = document.querySelector('#participentName')
+/** @format */
+
+const participentName = document.querySelector('#participentName');
+const participentNameShow = document.querySelector('#participentNameShow');
+const shuffle = document.querySelector('#shuffle');
+const first = document.querySelector('#first');
+const second = document.querySelector('#second');
+const third = document.querySelector('#third');
+
+let param = [];
+
+participentName.addEventListener('keypress', (e) => {
+	if (e.key === 'Enter') {
+		let newName = e.target.value.split(', ');
+
+		if (participentName.value !== '') {
+			newName.forEach((single) => {
+				param.push(single);
+				let item = madeul(single);
+				participentNameShow.appendChild(item);
+			});
+        } else {
+            alert('You must enter partifipend name')
+        }
+	}
+});
+
+function madeul(name) {
+	let li = document.createElement('li');
+	li.classList.add('list-group-item');
+	li.innerHTML = name;
+
+	return li;
+}
